7963d2fd63856be6d2c4b751bb1c656a6f9693c6
Surajit Karuri <surajitkaruri@gmail.com>
2020-10-25 02:26:00 +0530

    created app.js

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,9 @@
+const defaultResult = 0;
+let var1 = 0;
+let var2 = 0;
+let currentResult = defaultResult;
+defaultResult = 0;
+let calculationDescription = "desc";
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
+calculationDescription = `${((0 + 10) * 3) / 2 - 1}`;
+outputResult(currentResult, calculationDescription);
