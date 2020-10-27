f73e00f6b3add58d3f69f0c076ce3bc25e422411
Surajit Karuri <surajitkaruri@gmail.com>
2020-10-27 10:32:51 +0530

    added function operations

--- basics-01-starting-project/assets/scripts/vendor.js
+++ basics-01-starting-project/assets/scripts/vendor.js
@@ -1,11 +1,15 @@
-const userInput = document.getElementById('input-number');
-const addBtn = document.getElementById('btn-add');
-const subtractBtn = document.getElementById('btn-subtract');
-const multiplyBtn = document.getElementById('btn-multiply');
-const divideBtn = document.getElementById('btn-divide');
+const userInput = document.getElementById("input-number");
+const addBtn = document.getElementById("btn-add");
+const subtractBtn = document.getElementById("btn-subtract");
+const multiplyBtn = document.getElementById("btn-multiply");
+const divideBtn = document.getElementById("btn-divide");
 
-const currentResultOutput = document.getElementById('current-result');
-const currentCalculationOutput = document.getElementById('current-calculation');
+const currentResultOutput = document.getElementById("current-result");
+const currentCalculationOutput = document.getElementById("current-calculation");
+
+const userName = document.getElementById("userName");
+
+document.getElementById("greet").onclick = alert(`Hi ${userName}`);
 
 function outputResult(result, text) {
   currentResultOutput.textContent = result;
